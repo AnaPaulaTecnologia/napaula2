37108c57bccdfc224a29d4dd8e019787f0fac8d9
AnaPaulaTecnologia <00001094189492sp@al.educacao.sp.gov.br>
2024-11-08 12:09:18 -0300

    neyma

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Qual foi o maior Império existente?',
+    'Foi o império Romano'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Itália?',
+    'A capital da Itália é Roma'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é o maior país do mundo?',
+    'O maior país do mundo é a Rússia'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Oi em ingles é bye'
+)
